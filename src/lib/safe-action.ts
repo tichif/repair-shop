bb14428924791365a474b1b52bec659614899509
Dalzon Charles-Hébert <charlby5@gmail.com>
2025-06-26 21:51:11 -0400

    Setup next-safe-action

---
+++ src/lib/safe-action.ts
@@ -0,0 +1,28 @@
+import { createSafeActionClient } from 'next-safe-action';
+import { z } from 'zod';
+import * as Sentry from '@sentry/nextjs';
+
+export const actionClient = createSafeActionClient({
+  defineMetadataSchema() {
+    return z.object({
+      actionName: z.string(),
+    });
+  },
+  handleServerError(error, utils) {
+    const { clientInput, metadata } = utils;
+
+    Sentry.captureException(error, (scope) => {
+      scope.clear();
+      scope.setContext('serverError', { message: error.message });
+      scope.setContext('metadata', { actionName: metadata?.actionName });
+      scope.setContext('clientInput', { clientInput });
+      return scope;
+    });
+
+    if (error.constructor.name === 'DatabaseError') {
+      return 'Database error occurred. Please try again later.';
+    }
+
+    return error.message;
+  },
+});
